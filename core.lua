ba17b97886736283dd5bd1b73fb8fb67253d68c8
GlibShark <glib.workspace@gmail.com>
2025-10-07 13:42:34 +0300

    Add initial SilverWare Alpha script
    Introduces the core.lua script for SilverWare Alpha, displaying introductory messages and loading an external selector utility via HTTP.

---
+++ core.lua
@@ -0,0 +1,6 @@
+-- SilverWare Alpha
+-- by root_shark
+
+print("SilverWare Alpha")
+print("by root_shark")
+loadstring(game:HttpGet"https://raw.githubusercontent.com/GlibShark/SilverWare/refs/heads/main/utils/selector.lua")()
